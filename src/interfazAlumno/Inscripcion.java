625e488622efb6c37a72aaa1aa73b3cfb0bc4e5b
Jesus <Kechuu255@hotmail.com>
2019-08-02 18:39:41 -0300

    actualizacion de interfaz alumno

--- src/interfazAlumno/Inscripcion.java
+++ src/interfazAlumno/Inscripcion.java
@@ -5,6 +5,10 @@
  */
 package interfazAlumno;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
 import menu.Principal;
 
 /**
@@ -13,13 +17,86 @@ import menu.Principal;
  */
 public class Inscripcion extends javax.swing.JInternalFrame {
 
-    /**
-     * Creates new form InscripcionAlumno
-     */
+    Statement st;
+    ResultSet rs;
+    
     public Inscripcion() {
         initComponents();
+        
     }
 
+    //Cargar de combo box
+    
+    public void cargarComboLugarNacimiento(){
+        try {
+            Connection con = clases.Conectar.conexion();
+            st = con.createStatement();
+            rs = st.executeQuery("SELECT nombre FROM lugar WHERE nivel = 3");
+            
+            while (rs.next()) {                
+                cbxNacimiento.addItem(rs.getString("nombre"));
+            }
+            rs.close();
+            con.close();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getLocalizedMessage());
+        }
+    }
+    
+    public void cargarComboNacionalidad(){
+        try {
+            Connection con = clases.Conectar.conexion();
+            st = con.createStatement();
+            rs = st.executeQuery("SELECT detalle FROM nacionalidad");
+            
+            while (rs.next()) {                
+                cbxNacionalidad.addItem(rs.getString("detalle"));
+            }
+            rs.close();
+            con.close();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getLocalizedMessage());
+        }
+    }
+    
+    public void cargarComboDocumento(){
+        try {
+            Connection con = clases.Conectar.conexion();
+            st = con.createStatement();
+            rs = st.executeQuery("SELECT detalle FROM tipoDocumento");
+            
+            while (rs.next()) {                
+                cbxDocumento.addItem(rs.getString("detalle"));
+            }
+            rs.close();
+            con.close();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getLocalizedMessage());
+        }
+    }
+    
+    public void cargarComboLocalidad(){
+        try {
+            Connection con = clases.Conectar.conexion();
+            st = con.createStatement();
+            rs = st.executeQuery("SELECT nombre FROM lugar WHERE nivel = 4");
+            
+            while (rs.next()) {                
+                cbxNacimiento.addItem(rs.getString("nombre"));
+            }
+            rs.close();
+            con.close();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getLocalizedMessage());
+        }
+    }
+    
+    //Habilitar Combobox's
+    void botones(){
+        
+    }
+    
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -44,12 +121,12 @@ public class Inscripcion extends javax.swing.JInternalFrame {
         jPanel3 = new javax.swing.JPanel();
         femenino = new javax.swing.JRadioButton();
         masculino = new javax.swing.JRadioButton();
-        combonacimiento = new javax.swing.JComboBox<>();
+        cbxNacimiento = new javax.swing.JComboBox<>();
         jLabel15 = new javax.swing.JLabel();
-        combonacionalidad = new javax.swing.JComboBox<>();
+        cbxNacionalidad = new javax.swing.JComboBox<>();
         jLabel14 = new javax.swing.JLabel();
         jLabel13 = new javax.swing.JLabel();
-        combodocumento = new javax.swing.JComboBox<>();
+        cbxDocumento = new javax.swing.JComboBox<>();
         jLabel17 = new javax.swing.JLabel();
         txtnumerodocumento = new javax.swing.JTextField();
         txtcuil = new javax.swing.JTextField();
@@ -63,26 +140,26 @@ public class Inscripcion extends javax.swing.JInternalFrame {
         txtcorreo = new javax.swing.JTextField();
         jPanel7 = new javax.swing.JPanel();
         jLabel7 = new javax.swing.JLabel();
-        comboplanes = new javax.swing.JComboBox<>();
+        cbxPlanes = new javax.swing.JComboBox<>();
         jLabel25 = new javax.swing.JLabel();
-        combotrabajo = new javax.swing.JComboBox<>();
+        cbxTrabajo = new javax.swing.JComboBox<>();
         jLabel26 = new javax.swing.JLabel();
         txthijos1 = new javax.swing.JTextField();
         jPanel6 = new javax.swing.JPanel();
         jLabel6 = new javax.swing.JLabel();
-        combolocalidad = new javax.swing.JComboBox<>();
+        cbxLocalidad = new javax.swing.JComboBox<>();
         jLabel16 = new javax.swing.JLabel();
         txtcodigopostal = new javax.swing.JTextField();
         jLabel19 = new javax.swing.JLabel();
-        combobarrio = new javax.swing.JComboBox<>();
+        cbxBarrio = new javax.swing.JComboBox<>();
         jLabel21 = new javax.swing.JLabel();
-        combocalle = new javax.swing.JComboBox<>();
+        cbxCalle = new javax.swing.JComboBox<>();
         jLabel20 = new javax.swing.JLabel();
         txthijos = new javax.swing.JTextField();
         jLabel22 = new javax.swing.JLabel();
-        combotorre = new javax.swing.JComboBox<>();
+        cbxTorre = new javax.swing.JComboBox<>();
         jLabel23 = new javax.swing.JLabel();
-        combopiso = new javax.swing.JComboBox<>();
+        cbxPiso = new javax.swing.JComboBox<>();
         jLabel24 = new javax.swing.JLabel();
         txthijos2 = new javax.swing.JTextField();
         btnguardar1 = new javax.swing.JButton();
@@ -161,18 +238,18 @@ public class Inscripcion extends javax.swing.JInternalFrame {
 
         jPanel4.add(jPanel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(499, 29, -1, 57));
 
-        combonacimiento.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
-        combonacimiento.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "seleccionar" }));
-        combonacimiento.setToolTipText("");
-        jPanel4.add(combonacimiento, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 127, -1, -1));
+        cbxNacimiento.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        cbxNacimiento.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "seleccionar" }));
+        cbxNacimiento.setToolTipText("");
+        jPanel4.add(cbxNacimiento, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 127, -1, -1));
 
         jLabel15.setText("Lugar De Nacimiento");
         jPanel4.add(jLabel15, new org.netbeans.lib.awtextra.AbsoluteConstraints(18, 104, -1, -1));
 
-        combonacionalidad.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
-        combonacionalidad.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "seleccionar" }));
-        combonacionalidad.setToolTipText("");
-        jPanel4.add(combonacionalidad, new org.netbeans.lib.awtextra.AbsoluteConstraints(187, 127, -1, -1));
+        cbxNacionalidad.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        cbxNacionalidad.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "seleccionar" }));
+        cbxNacionalidad.setToolTipText("");
+        jPanel4.add(cbxNacionalidad, new org.netbeans.lib.awtextra.AbsoluteConstraints(187, 127, -1, -1));
 
         jLabel14.setText("Nacionalidad");
         jPanel4.add(jLabel14, new org.netbeans.lib.awtextra.AbsoluteConstraints(175, 104, -1, -1));
@@ -180,10 +257,10 @@ public class Inscripcion extends javax.swing.JInternalFrame {
         jLabel13.setText("Tipo Documento");
         jPanel4.add(jLabel13, new org.netbeans.lib.awtextra.AbsoluteConstraints(306, 104, -1, -1));
 
-        combodocumento.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
-        combodocumento.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "seleccionar" }));
-        combodocumento.setToolTipText("");
-        jPanel4.add(combodocumento, new org.netbeans.lib.awtextra.AbsoluteConstraints(318, 127, -1, -1));
+        cbxDocumento.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        cbxDocumento.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "seleccionar" }));
+        cbxDocumento.setToolTipText("");
+        jPanel4.add(cbxDocumento, new org.netbeans.lib.awtextra.AbsoluteConstraints(318, 127, -1, -1));
 
         jLabel17.setText("N° Documento");
         jPanel4.add(jLabel17, new org.netbeans.lib.awtextra.AbsoluteConstraints(437, 104, -1, -1));
@@ -230,17 +307,17 @@ public class Inscripcion extends javax.swing.JInternalFrame {
         jLabel7.setText("Planes");
         jPanel7.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(18, 29, -1, -1));
 
-        comboplanes.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
-        comboplanes.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Seleccionar" }));
-        jPanel7.add(comboplanes, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 53, -1, -1));
+        cbxPlanes.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        cbxPlanes.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Seleccionar" }));
+        jPanel7.add(cbxPlanes, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 53, -1, -1));
 
         jLabel25.setText("HIjos");
         jPanel7.add(jLabel25, new org.netbeans.lib.awtextra.AbsoluteConstraints(18, 83, -1, -1));
 
-        combotrabajo.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
-        combotrabajo.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "seleccionar" }));
-        combotrabajo.setToolTipText("");
-        jPanel7.add(combotrabajo, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 152, -1, -1));
+        cbxTrabajo.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        cbxTrabajo.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "seleccionar" }));
+        cbxTrabajo.setToolTipText("");
+        jPanel7.add(cbxTrabajo, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 152, -1, -1));
 
         jLabel26.setText("Trabajo");
         jPanel7.add(jLabel26, new org.netbeans.lib.awtextra.AbsoluteConstraints(18, 129, -1, -1));
@@ -257,9 +334,9 @@ public class Inscripcion extends javax.swing.JInternalFrame {
         jLabel6.setText("Localidad");
         jPanel6.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(18, 29, -1, -1));
 
-        combolocalidad.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
-        combolocalidad.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Seleccionar" }));
-        jPanel6.add(combolocalidad, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 20, -1, -1));
+        cbxLocalidad.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        cbxLocalidad.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Seleccionar" }));
+        jPanel6.add(cbxLocalidad, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 20, -1, -1));
 
         jLabel16.setText("Código postal");
         jPanel6.add(jLabel16, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 30, -1, -1));
@@ -270,16 +347,16 @@ public class Inscripcion extends javax.swing.JInternalFrame {
         jLabel19.setText("Barrio");
         jPanel6.add(jLabel19, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 70, -1, -1));
 
-        combobarrio.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
-        combobarrio.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Seleccionar" }));
-        jPanel6.add(combobarrio, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 90, -1, -1));
+        cbxBarrio.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        cbxBarrio.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Seleccionar" }));
+        jPanel6.add(cbxBarrio, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 90, -1, -1));
 
         jLabel21.setText("Calle");
         jPanel6.add(jLabel21, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 70, -1, -1));
 
-        combocalle.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
-        combocalle.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Seleccionar" }));
-        jPanel6.add(combocalle, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 90, -1, -1));
+        cbxCalle.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        cbxCalle.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Seleccionar" }));
+        jPanel6.add(cbxCalle, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 90, -1, -1));
 
         jLabel20.setText("N° de casa");
         jPanel6.add(jLabel20, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 70, -1, -1));
@@ -290,21 +367,21 @@ public class Inscripcion extends javax.swing.JInternalFrame {
         jLabel22.setText("Torre");
         jPanel6.add(jLabel22, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 140, -1, -1));
 
-        combotorre.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
-        combotorre.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Seleccionar" }));
-        combotorre.addActionListener(new java.awt.event.ActionListener() {
+        cbxTorre.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        cbxTorre.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Seleccionar" }));
+        cbxTorre.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                combotorreActionPerformed(evt);
+                cbxTorreActionPerformed(evt);
             }
         });
-        jPanel6.add(combotorre, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 160, -1, -1));
+        jPanel6.add(cbxTorre, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 160, -1, -1));
 
         jLabel23.setText("Piso");
         jPanel6.add(jLabel23, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 140, -1, -1));
 
-        combopiso.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
-        combopiso.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Seleccionar" }));
-        jPanel6.add(combopiso, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 160, -1, -1));
+        cbxPiso.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        cbxPiso.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Seleccionar" }));
+        jPanel6.add(cbxPiso, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 160, -1, -1));
 
         jLabel24.setText("N° de departamento");
         jPanel6.add(jLabel24, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 140, -1, -1));
@@ -364,9 +441,9 @@ public class Inscripcion extends javax.swing.JInternalFrame {
         dispose();
     }//GEN-LAST:event_btncancelarActionPerformed
 
-    private void combotorreActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_combotorreActionPerformed
+    private void cbxTorreActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cbxTorreActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_combotorreActionPerformed
+    }//GEN-LAST:event_cbxTorreActionPerformed
 
     private void btnguardarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnguardarActionPerformed
         // TODO add your handling code here:
@@ -384,16 +461,16 @@ public class Inscripcion extends javax.swing.JInternalFrame {
     private javax.swing.JButton btnguardar1;
     private javax.swing.JButton btntomarfoto;
     private javax.swing.JButton btnverificar;
-    private javax.swing.JComboBox<String> combobarrio;
-    private javax.swing.JComboBox<String> combocalle;
-    private javax.swing.JComboBox<String> combodocumento;
-    private javax.swing.JComboBox<String> combolocalidad;
-    private javax.swing.JComboBox<String> combonacimiento;
-    private javax.swing.JComboBox<String> combonacionalidad;
-    private javax.swing.JComboBox<String> combopiso;
-    private javax.swing.JComboBox<String> comboplanes;
-    private javax.swing.JComboBox<String> combotorre;
-    private javax.swing.JComboBox<String> combotrabajo;
+    private javax.swing.JComboBox<String> cbxBarrio;
+    private javax.swing.JComboBox<String> cbxCalle;
+    private javax.swing.JComboBox<String> cbxDocumento;
+    private javax.swing.JComboBox<String> cbxLocalidad;
+    private javax.swing.JComboBox<String> cbxNacimiento;
+    private javax.swing.JComboBox<String> cbxNacionalidad;
+    private javax.swing.JComboBox<String> cbxPiso;
+    private javax.swing.JComboBox<String> cbxPlanes;
+    private javax.swing.JComboBox<String> cbxTorre;
+    private javax.swing.JComboBox<String> cbxTrabajo;
     private javax.swing.JRadioButton femenino;
     private com.toedter.calendar.JDateChooser jDateChooser1;
     private javax.swing.JLabel jLabel1;
